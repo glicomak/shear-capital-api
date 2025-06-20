8730c5177267375a89538f39eb46903cbd00c739
glicomak <skewtron@gmail.com>
2025-06-20 18:50:15 +0530

    Parallelize simulation of day

--- simulation/main.py
+++ simulation/main.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from core.day import advance_day, current_day, target_day
 from models.person import Person
 from tqdm import tqdm
@@ -16,8 +17,8 @@ class Simulator:
         print(f"Simulating from day {start_day} to {end_day - 1} ...")
 
         for _ in tqdm(range(start_day, end_day)):
-            for person in persons:
-                person.simulate()
+            with ThreadPoolExecutor() as executor:
+                list(executor.map(lambda person: person.simulate(), persons))
             advance_day()
         
         for person in persons:
